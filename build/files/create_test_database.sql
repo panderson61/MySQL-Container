28b774e0c55f0ecaa986eb069869bab989f1f1e7
Paul Anderson <panderson@avetta.com>
2017-04-26 14:38:32 -0700

    SQL script for creating test database

---
+++ build/files/create_test_database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE test;
+USE test;
+CREATE TABLE 'name' (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
