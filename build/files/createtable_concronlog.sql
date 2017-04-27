e819033f2935ad0706e5e2f1affc081030c863c4
Paul Anderson <panderson@avetta.com>
2017-04-27 10:11:49 -0700

    Create createtable_concronlog.sql

---
+++ build/files/createtable_concronlog.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS pics_live;
+CREATE TABLE IF NOT EXISTS `contractor_cron_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `conID` int(11) NOT NULL,
+  `startDate` datetime DEFAULT NULL,
+  `runTime` int(11) DEFAULT '0',
+  `success` tinyint(4) NOT NULL DEFAULT '0',
+  `server` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `conID` (`conID`),
+  KEY `startDate` (`startDate`)
+) DEFAULT CHARSET=utf8 
